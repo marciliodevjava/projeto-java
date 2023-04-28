b3870fdab5592235e048aaa10c7646d785f72b90
marciliodevjava <marciliodevjava@gmail.com>
2023-04-28 17:57:32 -0300

    Projeto Java

---
+++ eureka-serve/src/main/java/br/com/eurekaserve/eurekaserve/EurekaServeApplication.java
@@ -0,0 +1,13 @@
+package br.com.eurekaserve.eurekaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServeApplication.class, args);
+	}
+
+}
