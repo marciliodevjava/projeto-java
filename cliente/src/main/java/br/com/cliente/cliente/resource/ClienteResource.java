bdf21b8d5dc823b1c0923dc43329d505e6e5f6e9
marciliodevjava <marciliodevjava@gmail.com>
2023-04-28 20:10:38 -0300

    Criação Class ClienteResource

---
+++ cliente/src/main/java/br/com/cliente/cliente/resource/ClienteResource.java
@@ -0,0 +1,25 @@
+package br.com.cliente.cliente.resource;
+
+import br.com.cliente.cliente.dto.ClienteDto;
+import br.com.cliente.cliente.service.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PutMapping("/cadastrar")
+    public ResponseEntity<ClienteDto> cadastrar(@RequestBody @Valid ClienteDto clienteDto){
+
+       return ResponseEntity.ok(clienteDto);
+    }
+}
