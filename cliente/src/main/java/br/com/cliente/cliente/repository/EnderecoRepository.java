dee794ec601866dc906025d1934c0afbb4a6477f
marciliodevjava <marciliodevjava@gmail.com>
2023-04-28 20:09:47 -0300

    Criação Class EnderecoRepository

---
+++ cliente/src/main/java/br/com/cliente/cliente/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.cliente.cliente.repository;
+
+import br.com.cliente.cliente.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
