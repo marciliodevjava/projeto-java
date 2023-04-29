37cb3d5d20f345d0de705e3855f522074571f5de
marciliodevjava <marciliodevjava@gmail.com>
2023-04-28 21:14:11 -0300

    Update ClienteDto

--- cliente/src/main/java/br/com/cliente/cliente/dto/ClienteDto.java
+++ cliente/src/main/java/br/com/cliente/cliente/dto/ClienteDto.java
@@ -1,4 +1,23 @@
 package br.com.cliente.cliente.dto;
 
+import br.com.cliente.cliente.domain.Endereco;
+import br.com.cliente.cliente.domain.enuns.EnumStadoCliente;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class ClienteDto {
+
+    private String nome;
+    private String cpfCnpj;
+    private String rg;
+    private String data;
+    private Long ddd;
+    private String telefone;
+    private String email;
+    private List<EnderecoDto> enderecoDto;
+    private EnumStadoCliente enumStadoCliente;
 }
